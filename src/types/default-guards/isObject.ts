cc02bd2b94ccf2ecf0f16367acf110dc00b2d7dc
Matthew Tramantano <matt.tramantano@gmail.com>
2023-04-05 12:15:49 -0600

    updated guard

--- src/types/default-guards/isObject.ts
+++ src/types/default-guards/isObject.ts
@@ -1,2 +1,15 @@
-const isObject = <T extends object>(val: any): val is T =>  typeof val === 'object' && typeof val !== 'function' && typeof val !== 'undefined';
+// const isObject = <T extends object>(val: any): val is T =>  typeof val === 'object' && typeof val !== 'function' && typeof val !== 'undefined';
+const isObject = <T extends object>(val: any): val is T => {
+    if (val === null || typeof val !== 'object' || typeof val === 'function' || typeof val === 'undefined') {
+        return false;
+    }
+    const reqKeyList: (keyof T)[] = Object.keys({} as Required<T>) as (keyof T)[];
+    for (const key of reqKeyList) {
+        if (!(key in val)) {
+            return false;
+        }
+    }
+    return true;
+};
+
 export default isObject;
